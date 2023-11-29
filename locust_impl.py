44cdd8ff126b377e0b91dd248a53e8aa1680a9c2
lee minseop <lms990108@gmail.com>
2023-11-29 22:29:50 +0900

    Feat: 검색기능에 대해서 초기 테스트

--- locust_impl.py
+++ locust_impl.py
@@ -1,25 +1,11 @@
 from locust import task, FastHttpUser
 
 class TargetURL:
-    ROOT = "/"
-    STRING = "/string"
-    JSON = "/json"
-    CALC = "/calc"
+    SEARCH = "/search"
 
 class LocustImpl(FastHttpUser):
 
     @task
-    def root(self):
-        self.client.get(TargetURL.ROOT)
-
-    @task
-    def string(self):
-        self.client.get(TargetURL.STRING)
-
-    @task
-    def json(self):
-        self.client.get(TargetURL.JSON)
-
-    @task
-    def calc(self):
-        self.client.get(TargetURL.CALC)
+    def search(self):
+        search_query = "사랑"  # 검색어 예시
+        self.client.get(TargetURL.SEARCH, params={"query": search_query})
